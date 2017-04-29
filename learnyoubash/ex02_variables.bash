30cdda8f20912fe73a0c2c869393c8b2b77cfc9a
nevosial <nevosial@gmail.com>
2017-04-29 23:49:49 +0530

    vars local and global

---
+++ learnyoubash/ex02_variables.bash
@@ -0,0 +1,11 @@
+echo "User $USER in directory $PWD."
+
+
+echo "---The current user\'s home directory : $HOME."
+echo "---The current user : $USER. "
+echo "---A colon-separated list of directories in which the shell looks for commands : $PATH."
+echo "---The current working directory : $PWD."
+echo "---Random integer between 0 and 32767 : $RANDOM."
+echo "---The numeric, real user ID of the current user : $UID."
+echo "---The primary prompt string : $PS1."
+echo "---The secondary prompt string : $PS2."  
