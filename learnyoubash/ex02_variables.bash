ff11c59e0e726f4c52e43e4c9f3822adf6b12589
Neville Mascarenhas <nevosial@gmail.com>
2017-06-01 10:13:59 +0530

    Update ex02_variables.bash

--- learnyoubash/ex02_variables.bash
+++ learnyoubash/ex02_variables.bash
@@ -1,6 +1,6 @@
 echo "User $USER in directory $PWD."
 
-
+echo "---List of basic commands---"
 echo "---The current user\'s home directory : $HOME."
 echo "---The current user : $USER. "
 echo "---A colon-separated list of directories in which the shell looks for commands : $PATH."
@@ -9,3 +9,4 @@ echo "---Random integer between 0 and 32767 : $RANDOM."
 echo "---The numeric, real user ID of the current user : $UID."
 echo "---The primary prompt string : $PS1."
 echo "---The secondary prompt string : $PS2."  
+echo "-----------------------------"
